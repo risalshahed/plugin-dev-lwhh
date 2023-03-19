0bddce1be987b02c994b1bf51f9aa2e9d8ea4188
risalshahed <risalshahed@gmail.com>
2023-03-19 14:46:43 +0600

    sec-6 added

---
+++ 6.2-3-category.php
@@ -0,0 +1,36 @@
+
+<!-- eikhane amra 1ta parameter PASS krte hook er pashapashi, tai THEME er "functions.php" file a ei ACTION ADD krar smy amra 1ta parameter PASS krte parbo -->
+<?php do_action('astra_category_page', single_cat_title('', false)); ?>
+
+<!-- data retrieve krte chaile true, just pass krte chaile false -->
+
+<?php get_header() ?>
+
+
+    <!-- s-content
+    ================================================== -->
+  <section class="s-content">
+
+    <div class="row narrow">
+      <div class="col-full s-content__header" data-aos="fade-up">
+        <!-- Start 6.2 (Filter Hook); see also "functions.php" -->
+          <?php // echo apply_filters("astra_text","hello world"); ?>
+          <?php echo apply_filters("astra_text","hello world", "gelu dunya"); ?>
+          
+          <?php do_action("astra_before_category_title"); ?>
+
+          <h1>
+            <?php single_cat_title(); ?>
+          </h1>
+
+          <?php do_action("astra_after_category_title"); ?>
+
+
+          <?php do_action("astra_before_category_description"); ?>
+
+          <p class="lead">
+            <?php echo category_description(); ?>
+          </p>
+
+          <?php do_action("astra_after_category_description"); ?>
+
